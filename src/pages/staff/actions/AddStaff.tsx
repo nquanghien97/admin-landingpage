c0debcfb8008cf06dc3369700550835844fc8fc8
QuangHien <nquanghien97@gmail.com>
2024-08-21 15:32:11 +0700

    fix staff api

--- src/pages/staff/actions/AddStaff.tsx
+++ src/pages/staff/actions/AddStaff.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useNotification } from '../../../hooks/useNotification';
-import { Form, Input, Modal, Image, Button, Select } from 'antd';
+import { Form, Input, Modal, Button, Select } from 'antd';
 import { StaffType } from '../../../entities/Staff';
 import { createStaff } from '../../../services/staff';
 
@@ -32,43 +32,9 @@ function AddStaff(props: AddStaffProps) {
   const { onClose, open, setRefreshKey } = props;
   const [form] = Form.useForm();
 
-  const [file, setFile] = useState<string>()
   const [loading, setLoading] = useState(false)
 
   const notification = useNotification()
-
-  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target || !e.target.files) return;
-    const file = e.target.files[0];  // Get the first (and only) file
-
-    const fileReader = new Promise<string>((resolve, reject) => {
-      const reader = new FileReader();
-
-      reader.onloadend = () => {
-        if (typeof reader.result === 'string') {
-          resolve(reader.result);
-        } else {
-          reject(new Error('Failed to read file as string'));
-        }
-      };
-
-      reader.onerror = () => {
-        reject(new Error('Failed to read file'));
-      };
-
-      reader.readAsDataURL(file);  // Read the single file as a data URL
-    });
-
-
-    fileReader
-      .then(base64String => {
-        // Handle the Base64 string, e.g., set it in state or upload to server
-        setFile(base64String); // Assuming you want to save the Base64 string in state
-      })
-      .catch(error => {
-        console.error('Error converting file to Base64:', error);
-      });
-  }
 
   const type = Form.useWatch('type', form);
 
@@ -77,7 +43,6 @@ function AddStaff(props: AddStaffProps) {
     const dataSubmit = {
       name: data.name,
       identifier: data.identifier ? data.identifier : undefined,
-      imageUrl: file,
       bankName: data.bankName ? data.bankName : undefined,
       bankNumber: data.bankNumber ? data.bankNumber : undefined,
       type: data.type
@@ -141,31 +106,6 @@ function AddStaff(props: AddStaffProps) {
           </div>
           {type === "IDENTIFIER" && (
             <>
-              <div className="flex items-center flex-col">
-                <div className="flex items-center w-full h-full">
-                  <p className="w-[120px] text-left text-[#0071BA]">Hình ảnh</p>
-                  <Form.Item
-                    className="!mb-0 w-full"
-                    name="imageUrl"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Trường này là bắt buộc"
-                      },
-                    ]}
-                  >
-                    <Input type="file" className="py-2" onChange={onFileChange} />
-                  </Form.Item>
-                </div>
-                {file && (
-                  <div className="flex flex-wrap justify-center w-full py-4 gap-4">
-                    <Image.PreviewGroup
-                    >
-                      <Image className="border-2 m-auto cursor-pointer" width={200} src={file} alt="preview avatar" />
-                    </Image.PreviewGroup>
-                  </div>
-                )}
-              </div>
               <div className="flex items-center h-[40px]">
                 <p className="w-[120px] text-left text-[#0071BA]">SĐT hoặc ID page</p>
                 <Form.Item
