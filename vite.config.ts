cde3bcda942823c767d4fe6ef55c898328721952
QuangHien <nquanghien97@gmail.com>
2024-08-27 13:50:59 +0700

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     host: true,
     proxy:{
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://unamocdon.vn',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
